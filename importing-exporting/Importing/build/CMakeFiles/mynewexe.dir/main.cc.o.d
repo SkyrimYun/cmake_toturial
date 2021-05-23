9034f45c024cfb9fbb50d4df1426cc6ad76f3a1e
SkyrimYun <skyrim-yy@outlook.com>
2021-05-23 20:52:19 +0200

    finish test

---
+++ importing-exporting/Importing/build/CMakeFiles/mynewexe.dir/main.cc.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/mynewexe.dir/main.cc.o: \
+ /home/yunfan/work_spaces/CMake/Help/guide/importing-exporting/Importing/build/main.cc \
+ /usr/include/stdc-predef.h
---
+++ importing-exporting/MathFunctions/build/CMakeFiles/MathFunctions.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MathFunctions.dir/src/MathFunctions.cxx.o"
+  "CMakeFiles/MathFunctions.dir/src/MathFunctions.cxx.o.d"
+  "libMathFunctions.a"
+  "libMathFunctions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MathFunctions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
